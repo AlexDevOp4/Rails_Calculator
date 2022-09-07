bf6f4c69290f7f7b831827d59128e842bfc7c884
Alex <alex.ashtiany@icloud.com>
2022-09-07 16:20:06 -0400

    added percentage and negative numbers

--- app/javascript/calculator.js
+++ app/javascript/calculator.js
@@ -1,31 +1,51 @@
 $(document).ready(function () {
 	let result = document.getElementById('result');
-	let equal = document.getElementById('equal');
 	let combinedNumbers;
-	let num1;
-	let num2;
+	let addedNumbers;
 
-	const add = (a, b) => {
-		let addedNumbers = a + b;
 
-		console.log(addedNumbers);
+	const add = (a) => {
+		let myArray = a.split('');
+
+		let newA = myArray.pop();
+
+		let combine = myArray.join('');
+
+		console.log(eval(combine));
+
+		addedNumbers = eval(combine);
 
 		result.innerHTML = addedNumbers;
+
+		return addedNumbers;
 	};
 
-	$('button').click(function () {
-		if (this.innerText === 'AC') return (result.innerText = '');
+	$('#negative').click(() => {
+		if (result.innerText == Math.abs(result.innerText)) {
+			return (result.innerHTML *= -1);
+		} else {
+			return (result.innerHTML = Math.abs(result.innerText));
+		}
+	});
+
+	$('.clicked-buttons').click(function () {
+
 		combinedNumbers = result.innerHTML += this.innerText;
 
 		return combinedNumbers;
 	});
 
-
-	$('#equal').click(function () {
-
-		if (combinedNumbers[1] === '+') {
-			add(parseFloat(combinedNumbers[0]), parseFloat(combinedNumbers[2]));
-		}
+	$('#reset').click(function () {
+		return (result.innerText = '');
 	});
 
+	$('#percentage').click(() => {
+		return (result.innerText = result.innerText / 100);
+	});
+
+	$('#equal').click(function () {
+		add(combinedNumbers);
+
+		return (result.innerHTML = addedNumbers);
+	});
 });
